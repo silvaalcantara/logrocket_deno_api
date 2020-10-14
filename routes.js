2a01f893738ebf5063a2a66e195124d459bfc5c2
silvaalcantara <allan.pa.br@gmail.com>
2020-10-14 19:25:25 -0300

    First commit

---
+++ routes.js
@@ -0,0 +1,17 @@
+import { Application } from "https://deno.land/x/oak@v4.0.0/mod.ts";
+import getBeers from "./controllers/getBeers.js";
+import getBeerDetails from "./controllers/getBeerDetails.js";
+import createBeer from "./controllers/createBeer.js";
+import updateBeer from "./controllers/updateBeer.js";
+import deleteBeer from "./controllers/deleteBeer.js";
+
+const router = new Router();
+
+router
+  .get("/beers", getBeers)
+  .get("/beers/:id", getBeerDetails)
+  .post("/beers", createBeer)
+  .put("/beers/:id", updateBeer)
+  .delete("/beers/:id", deleteBeer);
+
+export default router;
